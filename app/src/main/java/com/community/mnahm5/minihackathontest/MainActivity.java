bd718380f3872ac880a21dd069149c0be9467c43
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-04-15 01:21:40 +1000

    Initial commit

---
+++ app/src/main/java/com/community/mnahm5/minihackathontest/MainActivity.java
@@ -0,0 +1,69 @@
+package com.community.mnahm5.minihackathontest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ParseSetup();
+        setContentView(R.layout.activity_main);
+
+        ParseObject parseObject = new ParseObject("Test");
+        parseObject.put("name", "Nadim");
+        parseObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Test Success",
+                            Toast.LENGTH_LONG
+                    ).show();
+                    Log.i("Parse", "Success");
+                }
+                else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Test Failed\n" + e.getMessage(),
+                            Toast.LENGTH_LONG
+                    ).show();
+                    Log.i("Parse", "Failed " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void ParseSetup()
+    {
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("51757916800afeba27b718e6e9baeff132944e60")
+                .clientKey("b84609db03fa6ad0ad1899b372de38829a3ba1c8")
+                .server("http://ec2-54-252-158-169.ap-southeast-2.compute.amazonaws.com:80/parse/")
+                .build()
+        );
+
+        //ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
