ad335e61d33c00a061376200d60e5208d3f3eff1
Codurr <sthekken@gmail.com>
2017-04-23 11:50:04 +1000

    Added Clubs Async search

--- app/src/main/java/com/community/mnahm5/minihackathontest/ClubListFragment.java
+++ app/src/main/java/com/community/mnahm5/minihackathontest/ClubListFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,16 +48,28 @@ public class ClubListFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_club_list, container, false);
 
-        Button b = (Button) view.findViewById(R.id.button);
+        Button b = (Button) view.findViewById(R.id.btClubName);
         b.setOnClickListener(this);
+
+        Button c = (Button) view.findViewById(R.id.btSearchClubs);
+        c.setOnClickListener(this);
+
+        Button d = (Button) view.findViewById(R.id.btClearSearchClubs);
+        d.setOnClickListener(this);
 
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.button) {
+        if (v.getId() == R.id.btClubName) {
             addclubClicked(v);
+        }
+        if (v.getId() == R.id.btSearchClubs) { // For Club Search
+            searchClubs(v);
+        }
+        if (v.getId() == R.id.btClearSearchClubs) {
+            (new ClubListFragment.RemoteDataTask()).execute();
         }
     }
 
@@ -64,7 +77,7 @@ public class ClubListFragment extends Fragment implements View.OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new ClubListFragment.RemoteDataTask().execute();
+        (new ClubListFragment.RemoteDataTask()).execute();
     }
 
     // RemoteDataTask AsyncTask
@@ -100,8 +113,42 @@ public class ClubListFragment extends Fragment implements View.OnClickListener {
         }
     }
 
+
+    private class AsyncSearchTask extends AsyncTask<String, Void, Void> {
+        List<ParseObject> returned_object;
+        @Override
+        protected Void doInBackground(String... params) {
+            ParseQuery<ParseObject> query = new ParseQuery<>("Clubs");
+            query.whereContains("clubName", params[0]);
+            try {
+                returned_object = query.find();
+            } catch (ParseException e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            ListView listview = (ListView) getView().findViewById(R.id.lvClubView);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1);
+            for (ParseObject club : returned_object) {
+                adapter.add((String) club.get("clubName"));
+            }
+            listview.setAdapter(adapter);
+            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                }
+            });
+        }
+    }
+
+
     public void addclubClicked(View view){
-        TextView clubname = (TextView) getView().findViewById(R.id.etClubName);
+        EditText clubname = (EditText) getView().findViewById(R.id.etClubName);
         String current_user = ParseUser.getCurrentUser().getUsername();
         ParseObject clubs = new ParseObject("Clubs");
         clubs.put("clubName", clubname.getText().toString());
@@ -121,6 +168,13 @@ public class ClubListFragment extends Fragment implements View.OnClickListener {
                 }
             }
         });
+    }
+
+    public void searchClubs(View view){
+        EditText clubname = (EditText) getView().findViewById(R.id.etSearchClubs);
+
+        (new ClubListFragment.AsyncSearchTask()).execute(clubname.getText().toString());
+
     }
 
 }
